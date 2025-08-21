2bcc3b6e87418fd3fd305b3442371a64b89bc6fc
josh1024 <joshuakipamet@gmail.com>
2025-08-21 11:40:30 -0700

    Removed server and .vscode folders

--- server/routes/imageRouter.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import authUser from "../middleware/auth.js";
-import { generateImage } from "../controllers/imageController.js";
-
-const imageRouter = express.Router();
-
-imageRouter.post("/generate-image", authUser, generateImage);
-
-export default imageRouter;
