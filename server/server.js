576d557ba10e31054accba16fca9b4fae825cdee
josh1024 <joshuakipamet@gmail.com>
2025-08-22 20:50:40 -0700

    Update server and client files

--- server/server.js
+++ server/server.js
@@ -7,19 +7,19 @@ import paypalRoutes from "./routes/paypal.js";
 import cors from "cors";
 
 const app = express();
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors());
-connectToDB();
 
 app.use("/api/user", userRouter);
 app.use("/api/image", imageRouter);
 app.use("/paypal", paypalRoutes);
 app.get("/", (req, res) => {
-  console.log("Server is working");
+  res.send("Server is working");
 });
 
-app.listen(PORT, "0.0.0.0", () => {
+app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+  connectToDB();
 });
